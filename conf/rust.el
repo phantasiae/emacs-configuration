7a14d41e18079830467ce85f494f7f6315f6e345
fantasy <fantasy@rota>
2021-11-30 22:55:04 +0800

    cargo mode is first added.

--- conf/rust.el
+++ conf/rust.el
@@ -1,6 +1,7 @@
 ;;; rust  -- all configurations about rust
 ;;; Commentary:
 
+(require 'ivy)
 ;;; Code:
 (use-package lsp-mode
   :hook ((rust-mode . lsp))
@@ -8,12 +9,20 @@
   :ensure t)
 
 (use-package lsp-ivy
-  :commands lsp-ivy-workspace-symbol)
-
-(use-package rust-mode
   :ensure t)
 
+(use-package lsp-ivy
+  :commands lsp-ivy-workspace-symbol)
 
+(use-package cargo
+  :ensure t)
+
+(use-package rust-mode
+  :hook ((rust-mode . cargo-minor-mode)
+	 (rust-mode . ivy-mode))
+  :custom
+  (rust-format-on-save t)
+  :ensure t)
 
 (provide 'rust)
 ;;; rust.el ends here
