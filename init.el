3d6160c2bc2d14113e174713b085dc8e34e0cef0
fantasy <fantasy@rota>
2021-12-01 21:30:14 +0800

    auto saved config is added.

--- init.el
+++ init.el
@@ -13,14 +13,42 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-
-(add-to-list 'load-path "~/.emacs.d/conf/")
-
-(require 'use-package)
-(require 'support)
-(require 'web)
-(require 'android)
-(require 'rust)
-
+(eval-when-compile
+  (add-to-list 'load-path "~/.emacs.d/conf/")
+  (require 'use-package)
+  (require 'support)
+  (require 'web)
+  (require 'android)
+  (require 'rust))
 
 ;;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(auto-image-file-mode t)
+ '(column-number-mode t)
+ '(custom-safe-themes
+   '("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))
+ '(global-auto-revert-mode t)
+ '(global-font-lock-mode t)
+ '(inhibit-startup-screen t)
+ '(scroll-bar-mode nil)
+ '(show-paren-delay 0)
+ '(show-paren-mode t)
+ '(show-paren-style 'parenthesis)
+ '(tool-bar-mode nil)
+ '(url-automatic-caching t))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Cascadia Code" :foundry "SAJA" :slant normal :weight normal :height 98 :width normal)))))
